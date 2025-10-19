f7ad68c7aa5378aa6f3f32d04b75c10548a5ef89
Frank <feduvernal1423@gmail.com>
2025-10-19 13:56:30 -0600

    add configs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack(config) {
+    config.resolve.alias['@'] = __dirname;
+    return config;
+  },
 }
 
 export default nextConfig
